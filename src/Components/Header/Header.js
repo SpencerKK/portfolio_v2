f27fe98b6ec4e7cf881864d8ba0bb0e62241e267
SpencerKK <kenealys@me.com>
2020-07-30 06:42:18 -0700

    Initial Commit / Header Completed

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="header-left">
+                <h1>Spencer Kenealy</h1>
+                <p>Frontend Developer In Los Angeles</p>
+            </div>
+            <div className="header-right">
+                <i className="fab fa-github"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
